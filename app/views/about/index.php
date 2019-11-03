ddf398af5b5c054e17eb7d77b36c591777f9e250
WahyuS002 <syahputrawahyu61@gmail.com>
2019-11-03 21:41:01 +0700

    #6 Assets - Menambahkan asset berupa bootstrap(css, js) dan menambahkan file Constants.php yang menyimpan BASEURL berupa u rl untuk menghubungkan ke file asset

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-    
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL;?>/img/image.jpg" alt="Thanos" width="10%" class="rounded-circle shadow">
     <p>Nama Saya <?= $data["nama"]?></p>
+</div>
